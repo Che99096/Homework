7e0faa571600eadd89b8210fe532aa0160e93110
Oladotun Osisami <osidot@yahoo.com>
2022-04-14 21:14:36 -0500

    Perform Arithmetic Operation

---
+++ Arithmetic.py
@@ -0,0 +1,22 @@
+import math
+
+print()
+
+number = int(input("ENTER AN INTEGER :"))
+print("")
+
+Exponent = int(input("Enter any exponent  = """))
+square = math.pow(number,2)
+squareroot = math.sqrt(number)
+cuberoot = math.pow(number,1/3)
+powerofnumber = math.pow(number,Exponent)
+numberdivitself = number/number
+
+print("")
+
+print("The square of given number is: ",square)
+print("The square root of given number is: ",squareroot)
+print("The cuberoot of given number is: ",cuberoot)
+print("The given number to the given exponent is: ",powerofnumber)
+print("The given number divided by itself is: ",numberdivitself)
+
---
+++ Timezone.py
@@ -0,0 +1,11 @@
+import datetime
+
+print("")
+
+t = 6
+dt = datetime.datetime.today()
+
+tdelta = datetime.timedelta(hours=t)
+print("The date & time in Nigeria is : ", dt + tdelta)
+
+print("")
